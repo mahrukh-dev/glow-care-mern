fd4ec41c4de378950d7b81cdba7776c116133e89
MahRukh <mahrukhdev@gmail.com>
2024-05-10 15:15:16 +0500

    Routes set in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,40 @@
 import './App.css';
 import { Navbar } from './components/navbar/Navbar';
 
+import ShopCategory from './pages/ShopCategory';
+import ShopPage from './pages/ShopPage';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import LoginSignup from './pages/LoginSignup';
+
+
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import ContactUs from './pages/ContactUs';
+import Bundles from './pages/Bundles';
+import Reviews from './pages/Reviews';
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ShopPage />} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/shopcategory1" element={<ShopCategory category="category1"/>} />
+          <Route path="/shopcategory2" element={<ShopCategory category="category2"/>} />
+          <Route path="/shopcategory3" element={<ShopCategory category="category3"/>} />
+          <Route path="product/" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+          <Route path="/signup" element={<LoginSignup />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/bundles" element={<Bundles /> } />
+          <Route path="/reviews" element={<Reviews /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
