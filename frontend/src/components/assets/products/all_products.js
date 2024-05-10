c607a650094c0400746a14e0dac3f77b0004170b
MahRukh <mahrukhdev@gmail.com>
2024-05-10 11:19:17 +0500

    Products added

---
+++ frontend/src/components/assets/products/all_products.js
@@ -0,0 +1,83 @@
+import p1_img from './product_1.jpg';
+import p2_img from './product_2.png';
+import p3_img from './product_3.jpg';
+import p4_img from './product_4.jpg';
+import p5_img from './product_5.jpg';
+import p6_img from './product_6.jpg';
+import p7_img from './product_7.jpg';
+import p8_img from './product_8.jpg';
+import p9_img from './product_9.jpg';
+import p10_img from './product_10.jpg';
+
+let all_product = [
+    {
+        id: 1,
+        name: "Product 1",
+        category: "Category 1",
+        image: p1_img,
+        price: 100,
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        category: "Category 1",
+        image: p2_img,
+        price: 100,
+    },
+    {
+        id: 3,
+        name: "Product 3",
+        category: "Category 1",
+        image: p3_img,
+        price: 100,
+    },
+    {
+        id: 4,
+        name: "Product 4",
+        category: "Category 1",
+        image: p4_img,
+        price: 100,
+    },
+    {
+        id: 5,
+        name: "Product 5",
+        category: "Category 1",
+        image: p5_img,
+        price: 100,
+    },
+    {
+        id: 6,
+        name: "Product 6",
+        category: "Category 1",
+        image: p6_img,
+        price: 100,
+    },
+    {
+        id: 7,
+        name: "Product 7",
+        category: "Category 1",
+        image: p7_img,
+        price: 100,
+    },
+    {
+        id: 8,
+        name: "Product 8",
+        category: "Category 1",
+        image: p8_img,
+        price: 100,
+    },
+    {
+        id: 9,
+        name: "Product 9",
+        category: "Category 1",
+        image: p9_img,
+        price: 100,
+    },
+    {
+        id: 10,
+        name: "Product 10",
+        category: "Category 1",
+        image: p10_img,
+        price: 100,
+    },
+]
