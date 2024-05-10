8e391afbdbcbd91daa188802b5bc182bf1617f15
MahRukh <mahrukhdev@gmail.com>
2024-05-10 15:45:09 +0500

    GetStarted Component Added

---
+++ frontend/src/components/getStarted/GetStarted.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './GetStarted.css'
+
+//Main Image below the NavBar on Home Page
+
+export const GetStarted = () => {
+  return (
+    <div>GetStarted</div>
+  )
+}
