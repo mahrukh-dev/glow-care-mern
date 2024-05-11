0d9a054adf491495915d436291eb9ea3b6458fe0
quratulain08 <141954239+quratulain08@users.noreply.github.com>
2024-05-11 23:57:03 +0500

    Offers page added

---
+++ frontend/src/components/Offers/Offers.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Offers.css'
+import exlusive_image  from '../assets/products/product_1.png'
+
+const Offers = () => {
+    return(
+        <div className='offers'>
+               <div className="offers-left">
+                  <h1>Exclusive</h1>
+                  <h1>Offers For You</h1>
+                  <p>ONLY ON BEST SELLER PRODUCTS</p>
+                  <button>Check Now</button>
+               </div>
+               <div className="offers-left">
+                  <img src ={exlusive_image} alt="" />
+               </div>
+        </div>
+    )
+}
+
+export default Offers
--- frontend/src/pages/ShopPage.jsx
+++ frontend/src/pages/ShopPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { GetStarted } from '../components/getStarted/GetStarted'
 import Popular from '../components/popular/Popular'
+import Offers from '../components/Offers/Offers'
 
 const ShopPage = () => {
   return (
     <div>
       <GetStarted />
       <Popular />
+      <Offers />
     </div>
   )
 }
