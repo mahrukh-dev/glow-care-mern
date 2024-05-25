3a1764e980824b2ec5a09e5acdfc87976f713535
Javeria Rehman <jia.rehman122@gmail.com>
2024-05-26 02:25:32 +0500

    Carti Items component created

--- frontend/src/components/cartItems/CartItems.jsx
+++ frontend/src/components/cartItems/CartItems.jsx
@@ -20,7 +20,7 @@ const CartItems = () => {
             if(cartItems[e.id] > 0){
                 return(
                     <div>
-                <div className="cartitems-format">
+                <div className="cartitems-format cartitems-format-main">
                     <img src={e.image} alt="" className="carticon-product-icon"/>
                     <p>{e.name}</p>
                     <p>{e.price}</p>
