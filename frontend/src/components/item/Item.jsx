f4ec3a006823918c2cac967f34eebde0f0518479
MahRukh <mahrukhdev@gmail.com>
2024-05-25 22:49:52 +0500

    item component updated

--- frontend/src/components/item/Item.jsx
+++ frontend/src/components/item/Item.jsx
@@ -4,7 +4,9 @@ import './Item.css'
 const Item = (props) => {
     return (    
         <div className="item">
-            <img src={props.image} alt="" /> 
+            <Link to={`/product/${props.id}`}>
+                <img src={props.image} alt="" />
+            </Link> 
              <div className="item-details">
                 <h3>{props.name}</h3>
                 <p>${props.price}</p>
