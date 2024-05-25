64c1c9092fa378b68da8a3c64ff0d4d71fafbc9c
MahRukh <mahrukhdev@gmail.com>
2024-05-25 21:33:25 +0500

    shopContext to provide product data to all pages

---
+++ frontend/src/context/shopContext.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import all_products from "../components/assets/products/all_products";
+
+export const ShopContext = React.createContext(null);
+
+const ShopContextProvider = ( props ) => {
+    const contextValue = { all_products };
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    );
+}
+
+export default ShopContextProvider;
