570ac6cd023765180e41c625bc4d3f4997920a7c
MahRukh <mahrukhdev@gmail.com>
2024-05-26 21:46:20 +0500

    user signup endpoint

--- backend/index.js
+++ backend/index.js
@@ -103,10 +103,43 @@ const userSchema = new mongoose.Schema({
     cartData : { type: Object, default: {} },
     date: { type: Date, default: Date.now }
 });
-const User = mongoose.model('Users', userSchema);
+const Users = mongoose.model('Users', userSchema);
 
 //API for user registration
-app.post('/register', async (req, res) => {});
+app.post('/signup', async (req, res) => {
+    let check = await Users.findOne({ email: req.body.email });
+    if (check) {
+        return res.status(400).json(
+            { 
+                success: false, 
+                message: 'User already exists with the same email' 
+            }
+        );
+    }
+    let cart = {};
+    for (let i=0; i<300; i++){
+        cart[i] = 0;   
+    }
+    const user = new Users({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart,
+    });
+
+    await user.save();
+
+    //jwt authentication
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+
+    const token = jwt.sign(data, 'secret_glowcare');
+    res.json({ success: true, token});
+
+});
 
 app.listen(port, (error) => {
     if (!error) {
