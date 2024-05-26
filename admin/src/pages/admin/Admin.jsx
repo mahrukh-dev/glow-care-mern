17503338ffc16236c9d62c8825b2f9077a8cd437
MahRukh <mahrukhdev@gmail.com>
2024-05-26 20:31:08 +0500

    Routes set up

--- admin/src/pages/admin/Admin.jsx
+++ admin/src/pages/admin/Admin.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import './Admin.css';
 import Sidebar from "../../components/sidebar/Sidebar";
+import AddProduct from "../../components/addProduct/AddProduct";
+import ListProduct from "../../components/listProduct/ListProduct";
+import {Routes, Route} from 'react-router-dom';
 
 
 const Admin = () => {
     return (
         <div className="admin">
             <Sidebar />
+            <Routes>
+                <Route path="/addproducts" element={<AddProduct />} />
+                <Route path="/listproducts" element={<ListProduct />} />
+            </Routes>
         </div>
     )
 }
