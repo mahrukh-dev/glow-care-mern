f82349c5057055baa1e91b0bee58acc4e2001c5f
MahRukh <mahrukhdev@gmail.com>
2024-05-26 18:45:01 +0500

    upload endpoint successfully running

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,23 @@ app.get('/', (req, res) => {
 });
 
 //Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb)=>{
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+const upload = multer({storage: storage});
 
+// creating upload endpoint for images
+app.use('/images', express.static('upload/images'));
+app.post('/upload', upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    });
+    
+});
 
 app.listen(port, (error)=>{
     if(!error){
