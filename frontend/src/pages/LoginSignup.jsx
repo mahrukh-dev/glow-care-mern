3ed9da41abbafb09459041950ca046bba36613f1
Javeria Rehman <jia.rehman122@gmail.com>
2024-05-26 22:25:43 +0500

    Login and Sign up authentication done

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -1,18 +1,79 @@
 import React from 'react'
+
 import './css/LoginSignup.css'
+//import { set } from 'mongoose'
 
 const LoginSignup = (props) => {
+
+  const [state, setState] = React.useState("Login");
+  const [formData, setFormData] = React.useState({
+    username:"",  
+    password:"",
+    email:"",
+  })
+
+  const changeHandler = (e) => { //e is the event object
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  const login = async () => {
+    console.log("Login Function Executed", formData);
+
+    let responseData;
+    await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/form-data',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((response) => response.json()).then((data)=>responseData=data);
+
+    if(responseData.success){
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    }
+    else{
+      alert(responseData.errors);
+    }
+  }
+
+  const signup = async () => {
+    console.log("Signup Function Executed", formData);
+
+    let responseData;
+    await fetch('http://localhost:4000/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/form-data',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((response) => response.json()).then((data)=>responseData=data);
+
+    if(responseData.success){
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    }
+    else{
+      alert(responseData.errors);
+    }
+  }
+
   return (
     <div className="loginsignup">
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
+          {state==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name' />:<></>}
+          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Email Address' />
+          <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder='Password' />
         </div>
-        <button>Sign Up</button>
-        <p className='loginsignup-login'>Already have an account? <span>Log In</span></p>
+        <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
+        {state==="Sign Up"
+        ?<p className='loginsignup-login'>Already have an account? <span onClick={()=>{setState("Login")}}>Log In</span></p>
+        :<p className='loginsignup-login'>Create an account? <span
+        onClick={()=>{setState("Sign Up")}}>Click here</span></p>}
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" />
           <p>I agree to the <span>Terms & Conditions</span></p>
