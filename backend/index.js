6731f4dd1417d8fa60aa33bc1dde0c51d895946b
Javeria Rehman <jia.rehman122@gmail.com>
2024-05-26 22:56:55 +0500

    index file endpoint created

--- backend/index.js
+++ backend/index.js
@@ -94,6 +94,54 @@ app.get('/allproducts', async (req, res) => {
     res.send(products);
 });
 
+//creating middleware to fetch user
+const fetchUser = async (req, res, next) => {
+    const token = req.header('auth-token');
+    if (!token) {
+        res.status(401).send({errors: "Please authenticate using a valid token"})    
+    }
+    else {
+        try {
+          const data = jwt.verify(token, 'secret_glowcare');
+          req.user = data.user;
+          next();
+        } 
+        catch (error) {
+            res.status(401).send({errors: "Please authenticate using a valid token"})
+        }
+    }
+}
+
+//creating endpoint for adding products to cart
+app.post('/addtocart', fetchUser, async (req, res) => {
+    let userData = await Users.findOne({_id: req.user.id});
+    userData.cartData[req.body.itemId] += 1;
+    await Users.updateOneAndUpdate({_id: req.user.id}, {cartData: userData.cartData});
+    res.send("Added")
+});
+
+//creating endpoiny to remove product from cart
+app.post('/removefromcart', fetchUser, async (req, res) => {
+    console.log("removed", req.body.itemId);
+    let userData = await Users.findOne
+    ({
+        _id: req.user.id
+    }); 
+    if(userData.cartData[req.body.itemId] > 0){
+        userData.cartData[req.body.itemId] -= 1;
+    }
+    await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
+    res.send("Removed");
+});
+
+//creating endpoint to get cart data
+app.post('/getcart', fetchUser, async (req, res) => {
+    console.log("get cart");
+    let userData = await Users.findOne({ 
+        _id: req.user.id
+    });
+    res.json(userData.cartData);
+});
 
 //USER SCHEMA
 const userSchema = new mongoose.Schema({
