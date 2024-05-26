a7acffb850f4c5287478bcb05433d3ae3e4b34b8
MahRukh <mahrukhdev@gmail.com>
2024-05-26 22:31:19 +0500

    popular data through db

--- frontend/src/components/popular/Popular.jsx
+++ frontend/src/components/popular/Popular.jsx
@@ -1,15 +1,22 @@
 import React from "react"
 import './Popular.css'
-import all_products from '../assets/products/all_products'
 import Item from '../item/Item'
 
 const Popular = () => {
+    const [popular_products, setPopular_Products] = React.useState([]);
+    React.useEffect(() => {
+        fetch('http://localhost:4000/popular')
+        .then((response) => response.json())
+        .then((data) => {
+            setPopular_Products(data);
+        });
+    }, []);
     return (
         <div className="popular">
             <h1>POPULAR PRODUCTS</h1>
             <hr />
             <div className="popular-item">
-                {all_products.map((item, i) => {
+                {popular_products.map((item, i) => {
                     return <Item key={i} id={item.id} category={item.category} name={item.name} image={item.image} price={item.price}/>
                 })}
             </div>
