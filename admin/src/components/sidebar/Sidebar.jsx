2060f41662d62f400f87e9042b4c42444be1ed58
MahRukh <mahrukhdev@gmail.com>
2024-05-26 20:22:03 +0500

    sidebar implemented

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './Sidebar.css'
 import {Link} from 'react-router-dom'
+import add_product_icon from '../../assets/icon/add_product_icon.png'
+import list_product_icon from '../../assets/icon/list_product_icon.png'
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
         <Link to={'/addproducts'} style={{textDecoration:"none"}}>
-            <div className="sidebar-item">Add Products</div>
+            <div className="sidebar-item">
+                <img src={add_product_icon} alt=""/>
+                <p>Add Product</p>
+            </div>
+        </Link>
+        <Link to={'/listproducts'} style={{textDecoration:"none"}}>
+            <div className="sidebar-item">
+                <img src={list_product_icon} alt=""/>
+                <p>Product List</p>
+            </div>
         </Link>
     </div>
   )
