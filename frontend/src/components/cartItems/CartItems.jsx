f525f6148bc008b72d9ecc5d2264bc5879a2c494
Javeria Rehman <jia.rehman122@gmail.com>
2024-05-26 17:21:30 +0500

    Cart items css modified

--- frontend/src/components/cartItems/CartItems.jsx
+++ frontend/src/components/cartItems/CartItems.jsx
@@ -36,7 +36,7 @@ const CartItems = () => {
            })} 
            <div className="cartitems-down">
             <div className="cartitems-total">
-                <h1>cart totals</h1>
+                <h1>Cart Totals</h1>
                 <div>
                     <div className="cartitems-total-item">
                         <p>Subtotal</p>
