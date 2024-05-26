0a4994b75eef37fe40dee63f206164cc38e4a68e
MahRukh <mahrukhdev@gmail.com>
2024-05-26 19:06:52 +0500

    add product endpoint working

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const port = 3001;
+const port = 4000;
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -21,11 +21,11 @@ app.get('/', (req, res) => {
 //Image Storage Engine
 const storage = multer.diskStorage({
     destination: './upload/images',
-    filename: (req, file, cb)=>{
+    filename: (req, file, cb) => {
         return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
     }
 });
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
 // creating upload endpoint for images
 app.use('/images', express.static('upload/images'));
@@ -34,11 +34,46 @@ app.post('/upload', upload.single('product'), (req, res) => {
         success: 1,
         image_url: `http://localhost:${port}/images/${req.file.filename}`
     });
-    
+
 });
 
-app.listen(port, (error)=>{
-    if(!error){
+// Schema for Creating Products
+const productSchema = new mongoose.Schema({
+    id: { type: Number, required: true },
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    date: { type: Date, default: Date.now },
+    available: { type: Boolean, default: true },
+});
+const Product = mongoose.model('Product', productSchema);
+
+// API for Creating Products
+app.post('/addproducts', async (req, res) => {
+    const product = new Product({
+        id: req.body.id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        price: req.body.price,
+        description: req.body.description,
+    });
+    console.log(product);
+    //error handling
+
+    await product.save();
+    console.log("Product saved");
+    res.json({
+        success: true,
+        name: req.body.name,
+    });
+
+});
+
+app.listen(port, (error) => {
+    if (!error) {
         console.log('Server is running on port: ', port);
     }
     else {
