dfe4574bfb9b3ad68fadfa734f87715b22d1c2d1
MahRukh <mahrukhdev@gmail.com>
2024-05-26 22:11:00 +0500

    dymanic login button

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useRef, useContext, useState} from 'react'
+import React, { useRef, useContext, useState } from 'react'
 import './Navbar.css'
 import logo from '../assets/logo/glow_care_logo.png'
 import cart_icon from '../assets/icons/cart_icon.png'
@@ -13,48 +13,60 @@ export const Navbar = () => {
   const menuRef = useRef();
 
   const dropdown_toggle = (e) => {
-      menuRef.current.classList.toggle('nav-menu-visible');
-       e.target.classList.toggle('open');
-}
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
 
   return (
     <div className='navbar'>
-        <div className="nav-logo">
-            <img src={logo} alt="logo" />
-            <Link to='/' style={{textDecoration: 'none'}}>
-              <p onClick={()=>{setMenu("shop")}}>Glow Care</p>
-            </Link>
-        </div>
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
+        <Link to='/' style={{ textDecoration: 'none' }}>
+          <p onClick={() => { setMenu("shop") }}>Glow Care</p>
+        </Link>
+      </div>
 
-         <img className = 'nav-dropdown' onClick ={dropdown_toggle} src = {nav_dropdown} alt=" " />
+      <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt=" " />
 
-        <ul ref={menuRef} className="nav-menu">
-            <li onClick={()=>{setMenu("shop")}}>
-              <Link to='/' style={{textDecoration: 'none'}}>Shop</Link>
-              {menu==="shop"?<hr/>:<></>}
-            </li>
-            <li onClick={()=>{setMenu("bundles")}}>
-              <Link to='/bundles' style={{textDecoration: 'none'}}>Bundles</Link>
-              {menu==="bundles"?<hr/>:<></>}
-            </li>
-            <li onClick={()=>{setMenu("reviews")}}>
-              <Link to='/reviews' style={{textDecoration: 'none'}}>Reviews</Link>
-              {menu==="reviews"?<hr/>:<></>}
-            </li>
-            <li onClick={()=>{setMenu("contactus")}}>
-              <Link to='/contact-us' style={{textDecoration: 'none'}}>Contact Us</Link>
-              {menu==="contactus"?<hr/>:<></>}
-            </li>
-        </ul>
-        <div className="nav-login-cart">
-            <Link to='/login' style={{textDecoration: 'none'}}>
+      <ul ref={menuRef} className="nav-menu">
+        <li onClick={() => { setMenu("shop") }}>
+          <Link to='/' style={{ textDecoration: 'none' }}>Shop</Link>
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => { setMenu("bundles") }}>
+          <Link to='/bundles' style={{ textDecoration: 'none' }}>Bundles</Link>
+          {menu === "bundles" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => { setMenu("reviews") }}>
+          <Link to='/reviews' style={{ textDecoration: 'none' }}>Reviews</Link>
+          {menu === "reviews" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => { setMenu("contactus") }}>
+          <Link to='/contact-us' style={{ textDecoration: 'none' }}>Contact Us</Link>
+          {menu === "contactus" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        {
+          localStorage.getItem('auth-token')
+            ?
+            <button onClick={() => {
+              localStorage.removeItem('auth-token');
+              window.location.replace('/');
+            }}>
+              Logout
+            </button>
+            :
+            <Link to='/login' style={{ textDecoration: 'none' }}>
               <button>Login</button>
             </Link>
-            <Link to='/cart' style={{textDecoration: 'none'}}>
-              <img src={cart_icon} alt="cart" />
-            </Link>
-            <div className="nav-cart-count">{getTotalCartItems()}</div>
-        </div>
+        }
+
+        <Link to='/cart' style={{ textDecoration: 'none' }}>
+          <img src={cart_icon} alt="cart" />
+        </Link>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
+      </div>
     </div>
   )
 }
