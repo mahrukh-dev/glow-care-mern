e2a93312132b4b93f8af88e45b21721a423938d9
Javeria Rehman <jia.rehman122@gmail.com>
2024-05-26 23:32:48 +0500

    side bar in admin panel updated

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -3,6 +3,7 @@ import './Sidebar.css'
 import {Link} from 'react-router-dom'
 import add_product_icon from '../../assets/icon/add_product_icon.png'
 import list_product_icon from '../../assets/icon/list_product_icon.png'
+import request_icon from '../../assets/icon/patient_requests.png'
 
 const Sidebar = () => {
   return (
@@ -17,6 +18,12 @@ const Sidebar = () => {
             <div className="sidebar-item">
                 <img src={list_product_icon} alt="Product List"/>
                 <p>Product List</p>
+            </div>
+        </Link>
+        <Link to={'/patientrequests'} style={{textDecoration:"none"}}>
+            <div className="sidebar-item">
+                <img src={request_icon} alt="Patient Requests"/>
+                <p>Patient Requests</p>
             </div>
         </Link>
     </div>
